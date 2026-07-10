b8fda47c0fbc1291d9465839d79b7ca9a3ed780b
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-07-10 18:32:37 +0300

    clicks

---
+++ src/main/js/main.js
@@ -0,0 +1,7 @@
+
+const StartItems = document.querySelector('#StartItems')
+StartItems.addEventListener('click', (e) => {
+    const item = e.target.closest('[data-id]')
+    if (!item || !StartItems.contains(item)) return
+    console.log('clicked', item.dataset.id)
+})
