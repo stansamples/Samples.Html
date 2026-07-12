5d5abcb4b2fdd38915d73b4f3f75c638e624783e
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-07-12 15:51:55 +0300

    selected

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,7 +1,19 @@
+let selected = 'StartItems:foo'
 
 const StartItems = document.querySelector('#StartItems')
+
+function renderStartItems() {
+  StartItems.querySelectorAll('.StartItem').forEach((el) => {
+    el.classList.toggle('selected', el.dataset.id === selected)
+  })
+}
+
 StartItems.addEventListener('click', (e) => {
     const item = e.target.closest('.StartItem')
     if (!item || !StartItems.contains(item)) return
     console.log('clicked', item.dataset.id)
+    selected = item.dataset.id
+    renderStartItems()
 })
+
+renderStartItems()
