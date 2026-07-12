ba64db685dcefbaa9b33023b79e9d24bb28d3956
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-07-12 16:15:21 +0300

    colors

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,19 +1,26 @@
-let selected = 'StartItems:foo'
+let _selected = 'StartItems:foo'
+let _colors = 'dark'
 
-const StartItems = document.querySelector('#StartItems')
+const StartItems = document.getElementById('StartItems')
+const Colors = document.getElementById('Colors')
 
 function renderStartItems() {
-  StartItems.querySelectorAll('.StartItem').forEach((el) => {
-    el.classList.toggle('selected', el.dataset.id === selected)
+  StartItems.querySelectorAll('.StartItem').forEach((it) => {
+    it.classList.toggle('selected', it.dataset.id === _selected)
   })
 }
 
-StartItems.addEventListener('click', (e) => {
-    const item = e.target.closest('.StartItem')
-    if (!item || !StartItems.contains(item)) return
+StartItems.addEventListener('click', (event) => {
+    const item = event.target.closest('.StartItem')
     console.log('clicked', item.dataset.id)
-    selected = item.dataset.id
+    _selected = item.dataset.id
     renderStartItems()
+})
+
+Colors.addEventListener('click', (event) => {
+    const newColors = _colors === 'dark' ? 'light' : 'dark'
+    _colors = newColors
+    document.documentElement.setAttribute('data-colors', newColors)
 })
 
 renderStartItems()
