95cc5fff968b1722d6b771a1462de96858f8d278
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-07-12 14:50:48 +0300

    hover

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,7 +1,7 @@
 
 const StartItems = document.querySelector('#StartItems')
 StartItems.addEventListener('click', (e) => {
-    const item = e.target.closest('[data-id]')
+    const item = e.target.closest('.StartItem')
     if (!item || !StartItems.contains(item)) return
     console.log('clicked', item.dataset.id)
 })
